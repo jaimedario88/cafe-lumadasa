278b5efe3b1504d2093dc697086add1d43dd6677
Jaime Jaramillo <jaimedario88@gmail.com>
2026-04-20 22:10:31 -0500

    chore: implement Prettier with go template support and reformated files

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,7 +1,7 @@
 import { NavigationController } from './components/Navigation';
 import { ScrollAnimations } from './components/Animations';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
     new NavigationController();
     new ScrollAnimations('.reveal-up');
 });
